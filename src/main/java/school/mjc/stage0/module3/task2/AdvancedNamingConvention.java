4a0f48f4cb7a010556a05dddb83a6aa2c1a215b9
litim smail <litimsmail@gmail.com>
2024-08-13 11:58:41 +0100

    UpDate

--- src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
@@ -1,12 +1,7 @@
 package school.mjc.stage0.module3.task2;
 
 public class AdvancedNamingConvention {
-    private static final int ADULT_AGE = 18;
-
-    private int age;
-    private int phoneNumber;
-
-    void CAllToFriend() {
+    void callToFriend() {
         //some code
     }
 
--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -2,11 +2,6 @@ package school.mjc.stage0.module3.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int variable = 10;
-        int variable_One = 1;
-        int variableTwo = 2;
-        int variable_three = 3;
-        final int VARIABLEFOUR = 4;
         final int VARIABLEfIVE = 5;
         int VariableSix = 6;
         int variableseven = 7;
